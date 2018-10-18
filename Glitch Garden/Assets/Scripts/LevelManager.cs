bb1c1e9f2e4a45120dae3755e035b84736c21753
Adam-Thain <Adam.r.thain@gmail.com>
2018-10-18 19:15:13 +0100

    Initial Source Code Commit
    Added:
    
    -Splash Scene
    -Start Menu Scene
    -Level 1 Scene
    -Level 2 Scene
    -Level 3 Scene
    -Win Scene
    -Lose Scene
    -Options Scene
    - Level Manager script
    - Music Manager script

---
+++ Glitch Garden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    #region public floats
+
+    /// <summary>
+    /// Time between next level load
+    /// </summary>
+    public float autoLoadNextLevelAfter;
+
+    #endregion
+
+    #region Constructor
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public void Start()
+    {
+        // If the first scene is zero (start)
+        // Else load the next level automatically 
+        if(autoLoadNextLevelAfter == 0)
+            Debug.Log("level Auto load Disabled");
+        else
+            Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Load level or scenes
+    /// </summary>
+    /// <param name="name"> name of level </param>
+    public void LoadLevel(string name)
+    {
+        // Load the level
+        SceneManager.LoadScene(name);
+    }
+
+    /// <summary>
+    /// Request to quit the current game
+    /// </summary>
+	public void QuitRequest()
+    {
+        // Quit the game
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Load the next level
+    /// </summary>
+    public void LoadNextLevel()
+    {
+        // Load the next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    #endregion
+}
---
+++ Glitch Garden/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+    #region Private Members
+
+    /// <summary>
+    /// Audio Source Holder
+    /// </summary>
+    private AudioSource audioSource;
+
+    #endregion
+
+    #region Public Members
+
+    /// <summary>
+    /// Array of music change audio clips
+    /// </summary>
+    public AudioClip[] levelMusicChangeArray;
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// When the game starts
+    /// </summary>
+    public void Awake()
+    {
+        // Do not destroy this object when the game starts
+        DontDestroyOnLoad(gameObject);
+
+        Debug.Log("Dont destroy on load: " + name);
+    }
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    public void Start()
+    {
+        // Get the audio source component 
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// When the level or scene is loaded
+    /// </summary>
+    /// <param name="level"> Current level </param>
+    public void OnLevelWasLoaded(int level)
+    {
+        // Get the music based on the current level or scene
+        AudioClip thisLevelMusic = levelMusicChangeArray[level];
+
+        Debug.Log("Playing Clip: " + thisLevelMusic);
+
+        // If there is music attached
+        if (thisLevelMusic)
+        {
+            // Set the audio source clip to this level music
+            audioSource.clip = thisLevelMusic;
+
+            // Loop the audio source
+            audioSource.loop = true;
+
+            // Play the audio source
+            audioSource.Play();
+        }
+    }
+
+    #endregion
+}
