ba07062847ac3a73f35d3d6eb88d3e93bd833c67
Adam-Thain <Adam.r.thain@gmail.com>
2019-01-21 02:13:03 +0000

    New Features Added
    Added hitboxes:
    Attackers
    Defenders
    
    Added new keyframe from gravestone to smooth the animation out.
    
    Added debugs for collisions

---
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour {
+
+    [Range(-1f, 1.5f)]
+    public float walkSpeed;
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start () {
+        Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>();
+        myRigidbody.isKinematic = true;
+	}
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update () {
+        transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+	}
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void OnTriggerEnter2D (){
+        Debug.Log(name + " trigger enter");
+    }
+}
---
+++ Glitch Garden/Assets/Scripts/Defender.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Defender : MonoBehaviour {
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start () {
+	
+	}
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update () {
+	
+	}
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void OnTriggerEnter2D()
+    {
+        Debug.Log(name + " trigger enter");
+    }
+}
