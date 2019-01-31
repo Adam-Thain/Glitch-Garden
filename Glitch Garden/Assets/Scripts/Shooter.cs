27672600c8ae07d396516e3b887ff1f2bd046307
Adam-Thain <Adam.r.thain@gmail.com>
2019-01-31 23:33:46 +0000

    New Features Added/Removed:
    Removed Projectiles Prefab
    Updated Shooter.CS to Create new projectiles object if one isnt available
    Fired projectiles are added to projectiles object.

--- Glitch Garden/Assets/Scripts/Shooter.cs
+++ Glitch Garden/Assets/Scripts/Shooter.cs
@@ -9,6 +9,22 @@ public class Shooter : MonoBehaviour {
     public GameObject projectile, projectileParent,gun;
 
     /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start()
+    {
+        // find projectileparent
+        projectileParent = GameObject.Find("Projectiles");
+
+        // If projectile parent cannot be found crate a new projectiles gameobject for it
+        if(!projectileParent)
+        {
+            projectileParent = new GameObject("Projectiles");
+
+        }
+    }
+
+    /// <summary>
     /// Fire a projectile
     /// </summary>
     private void Fire()
