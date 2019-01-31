39ddbb2be957457a572e91b19fc7d0403c92996a
Adam-Thain <Adam.r.thain@gmail.com>
2019-01-31 02:45:23 +0000

    New Features Added:
    Static Selected defender class for when you select a defender tower.
    Added Background Panel
    Added Buttons
    Added Button.cs Script

---
+++ Glitch Garden/Assets/Button.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+    public GameObject defenderPrefab;
+    public static GameObject selectedDefender;
+
+    private Button[] buttonArray;
+    
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start () {
+        buttonArray = GameObject.FindObjectsOfType<Button>();
+	}
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update () {
+	
+	}
+
+    /// <summary>
+    /// When the mouse is clicked
+    /// </summary>
+    void OnMouseDown()
+    {
+        foreach(Button thisButton in buttonArray)
+        {
+            thisButton.GetComponent<SpriteRenderer>().color = Color.black;
+        }
+
+        GetComponent<SpriteRenderer>().color = Color.white;
+        selectedDefender = defenderPrefab;
+        print(selectedDefender);
+    }
+}
