053888b5ec7af289d000f5142db2a5ea2b18ab4c
Adam-Thain <Adam.r.thain@gmail.com>
2019-02-03 02:49:45 +0000

    New Features Added
    Added Animators and Spawners to shooter.cs.
    Added attacker position detection method.

--- Glitch Garden/Assets/Scripts/Shooter.cs
+++ Glitch Garden/Assets/Scripts/Shooter.cs
@@ -14,10 +14,22 @@ public class Shooter : MonoBehaviour {
     private GameObject projectileParent;
 
     /// <summary>
+    /// 
+    /// </summary>
+    private Animator animator;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private Spawner myLaneSpawner;
+
+    /// <summary>
     /// Use this for initialization
     /// </summary>
     void Start()
     {
+        animator = GameObject.FindObjectOfType<Animator>();
+
         // find projectileparent
         projectileParent = GameObject.Find("Projectiles");
 
@@ -25,8 +37,72 @@ public class Shooter : MonoBehaviour {
         if(!projectileParent)
         {
             projectileParent = new GameObject("Projectiles");
-
         }
+
+        SetMyLaneSpawner();
+        print(myLaneSpawner);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void Update()
+    {
+        // IF
+        // ELSE
+        if(IsAttackerAheadInLane())
+        {
+            animator.SetBool("isAttacking",true);
+        }
+        else
+        {
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void SetMyLaneSpawner()
+    {
+        Spawner[] spawnerArray = GameObject.FindObjectsOfType<Spawner>();
+
+        foreach(Spawner spawner in spawnerArray)
+        {
+            if (spawner.transform.position.y == transform.position.y)
+            {
+                myLaneSpawner = spawner;
+                return;
+            }
+        }
+
+        Debug.LogError(name + "Cant find Spawner in lane");
+    }
+    
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    bool IsAttackerAheadInLane()
+    {
+        // IF
+        if(myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+
+        // IF
+        foreach (Transform attacker in myLaneSpawner.transform)
+        {
+            if(attacker.transform.position.x > transform.position.x)
+            {
+                return true;
+            }
+        }
+
+        //
+        return false;
     }
 
     /// <summary>
