9c9fe4064cd545a7072f8ec0cc0dcf50f070dda3
Adam-Thain <Adam.r.thain@gmail.com>
2019-02-03 19:35:05 +0000

    New Features Added
    Added StarDisplay Text Element.
    Added StarDisplay.cs
    Added Add and Use Star methods
    Added Star method calls to star trophy

--- Glitch Garden/Assets/Scripts/Defender.cs
+++ Glitch Garden/Assets/Scripts/Defender.cs
@@ -3,6 +3,24 @@ using System.Collections;
 
 public class Defender : MonoBehaviour {
 
-    // Only used as a tag for now
+    /// <summary>
+    /// 
+    /// </summary>
+    private StarDisplay starDisplay;
 
+    /// <summary>
+    /// 
+    /// </summary>
+    void Start() {
+        starDisplay = GameObject.FindObjectOfType<StarDisplay>();
+    }
+
+    /// <summary>
+    /// Only used as a tag for now
+    /// </summary>
+    /// <param name="amount"></param>
+    public void AddStars (int amount)
+    {
+        starDisplay.AddStars(amount);
+    }
 }
---
+++ Glitch Garden/Assets/StarDisplay.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof(Text))]
+public class StarDisplay : MonoBehaviour {
+
+    private Text starText;
+    private int stars = 0;
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start () {
+        starText = GetComponent<Text>();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="amount"></param>
+    public void AddStars(int amount)
+    {
+        stars += amount;
+        UpdateDisplay();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="amount"></param>
+    public void UseStars(int amount)
+    {
+        stars -= amount;
+        UpdateDisplay();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void UpdateDisplay()
+    {
+        starText.text = stars.ToString();
+    }
+}
