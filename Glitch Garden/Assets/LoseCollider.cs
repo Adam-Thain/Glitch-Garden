315cec1a07f0023d6e6eeb404dedfb8fb2fc1e24
Adam-Thain <Adam.r.thain@gmail.com>
2019-02-05 01:54:48 +0000

    New Features Added
    Added Lose Collider
    Added LoseCollider.CS
    Added Level Manager to all levels

---
+++ Glitch Garden/Assets/LoseCollider.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+    private LevelManager levelManager;
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start ()
+    {
+        //
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void OnTriggerEnter2D()
+    {
+        //
+        levelManager.LoadLevel("07 Lose Screen");
+    }
+}
