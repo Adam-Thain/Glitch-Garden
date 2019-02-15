7acca62ccb23478b32c67a8a907b1fdc7518dc15
Adam-Thain <Adam.r.thain@gmail.com>
2019-02-15 02:39:13 +0000

    New Features Added:
    Added Timer Slider,
    Added Game Slider.cs Script

---
+++ Glitch Garden/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Tooltip("Our Level timer in SECONDS")]
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField] float levelTime = 10;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Update()
+    {
+        //
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelTime;
+
+        //
+        bool timerFinished = (Time.timeSinceLevelLoad >= levelTime);
+
+        //
+        if (timerFinished)
+        {
+            Debug.Log("Level Timer Expired");
+        }
+    }
+}
