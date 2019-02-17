1018071f73f609cf45bdab4199e091c61245fea5
Adam-Thain <Adam.r.thain@gmail.com>
2019-02-17 02:19:19 +0000

    New Features Added:
    Added MusicPlayer.cs
    Added OptionsController.cs
    Added PlayerPrefsController.cs
    Created save and navigate methods for options
    Added player preferences for music and difficulty

---
+++ Glitch Garden/Assets/Scripts/OptionsController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+
+    [SerializeField] float defaultVolume = 0.8f;
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+        //
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        //
+        var musicPlayer = FindObjectOfType<MusicPlayer>();
+
+        // IF
+        // ELSE
+        if (musicPlayer)
+        {
+            musicPlayer.SetVolume(volumeSlider.value);
+        }
+        else
+        {
+            Debug.LogWarning("No music player found, start from splash screen?");
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void SaveAndExit()
+    {
+        //
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+
+        //
+        FindObjectOfType<LevelLoader>().LoadMainMenu();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void SetDefaults()
+    {
+        //
+        volumeSlider.value = defaultVolume;
+    }
+}
---
+++ Glitch Garden/Assets/Scripts/PlayerPrefsController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsController : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    const string MASTER_VOLUME_KEY = "master volume";
+
+    /// <summary>
+    /// 
+    /// </summary>
+    const string DIFFICULTY_KEY = "difficulty";
+
+    /// <summary>
+    /// 
+    /// </summary>
+    const float MIN_VOLUME = 0f;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    const float MAX_VOLUME = 1f;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="volume"></param>
+    public static void SetMasterVolume(float volume)
+    {
+        // IF
+        // ELSE
+        if(volume >= MIN_VOLUME && volume <= MAX_VOLUME)
+        {
+            //
+            Debug.Log("Master volume set to " + volume);
+
+            //
+            PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+        }
+        else
+        {
+            //
+            Debug.LogError("Master volume is out of range");
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public static float GetMasterVolume()
+    {
+        //
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+    }
+}
